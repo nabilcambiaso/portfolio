ba31304f08db51c00184116201af9fee31832074
nabil <nabilcambiaso@gmail.com>
2021-05-21 23:55:50 +0100

    adding certif

---
+++ js/copy.js
@@ -0,0 +1,12 @@
+//copy the paragraph innerText onClick
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+    id="."+id;
+    $(id).html('&nbsp; Copied');
+}
